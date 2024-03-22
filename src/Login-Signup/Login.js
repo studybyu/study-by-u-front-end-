de946bbd3561811020dd1f4b76d1aaca04e81512
OnShubham <shubhamkharche3005@gmail.com>
2024-03-22 22:04:59 +0530

    updated

--- src/Login-Signup/Login.js
+++ src/Login-Signup/Login.js
@@ -1,50 +1,79 @@
-import React, { useState } from 'react';
-import { Form, Button, Container, Row, Col, Card } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import icon from "../assets/Kids Studying from Home-bro.png";
 
 const Login = () => {
   const [loginInfo, setLoginInfo] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
 
-  // Handle input change
   const handleChange = (e) => {
     const { name, value } = e.target;
     setLoginInfo({ ...loginInfo, [name]: value });
   };
 
-  // Handle form submit
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // Here you would validate the input and then send a login request to your authentication service
-    console.log('Login Request', loginInfo);
-    // Upon successful login, you might redirect the user to a different page or perform some other action
+    console.log("Login Request", loginInfo);
   };
 
   return (
-    <Container className="mt-5 ">
-      <Row className="justify-content-md-center">
-        <Col xs={12} md={6}>
+    <Container className="p-5">
+      <Row className="justify-content-md-center align-items-center login-background">
 
-        <Card className="p-4" style={{ borderRadius: '1rem' }} shadow="sm">
+      <Col xs={16} md={6} className="pb-4" >
+        <h1 className="text-center" style={{color: "white", font: "3rem"}}>Sign-In</h1>
+          <div className="login-card">
+            
+            <p className="" style={{color: "white", wordSpacing: "2px"}}>Login to your account</p>
 
-          <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" name="email" placeholder="Enter email" value={loginInfo.email} onChange={handleChange} required />
-            </Form.Group>
+            <hr />
+            
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  placeholder="Enter email"
+                  value={loginInfo.email}
+                  onChange={handleChange}
+                  required
+                />
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" name="password" placeholder="Password" value={loginInfo.password} onChange={handleChange} required />
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={loginInfo.password}
+                  onChange={handleChange}
+                  required
+                />
+              </Form.Group>
 
-            <Button variant="primary" type="submit">
-              Login
-            </Button>
-          </Form>
-          </Card>
+              <Button variant="primary" type="submit">
+                Login
+              </Button>
+            </Form>
+          </div>
         </Col>
+
+
+
+        {/*  */}
+        <Col
+          xs={12}
+          md={6}
+          className="d-flex justify-content-center align-items-center"
+        >
+          <img src={icon} alt="icon" className="img-fluid p-4" />
+        </Col>
+
+      
       </Row>
     </Container>
   );
